25a950fbb947d34df9cc11b5cce2ef857fc74f9f
Nishkal <nishkal8463@gmail.com>
2019-12-14 13:27:49 +0530

    Updates

---
+++ precache-manifest.e4af5eae5311fe6bcbdb312f6fcbfbe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03245287492475e6c3fdc7d81ce630b",
+    "url": "/react-redux-pro/index.html"
+  },
+  {
+    "revision": "4797902c017503aa2f12",
+    "url": "/react-redux-pro/static/css/main.6cfecf80.chunk.css"
+  },
+  {
+    "revision": "df3a610cdbc6f3c5d7cc",
+    "url": "/react-redux-pro/static/js/2.55483585.chunk.js"
+  },
+  {
+    "revision": "4797902c017503aa2f12",
+    "url": "/react-redux-pro/static/js/main.8cb0a800.chunk.js"
+  },
+  {
+    "revision": "ecdd1e17a7ebbcec9e9d",
+    "url": "/react-redux-pro/static/js/runtime-main.ddd8bf35.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-redux-pro/static/media/burger-logo.b8503d26.png"
+  }
+]);
